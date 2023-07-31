e8cfb0db0f30e90dc2fdc66614a57bfdd73b27db
Matthieu Daulhiac <96766017+MatthieuD134@users.noreply.github.com>
2023-07-31 10:47:51 +0800

    add link for bundles in localhost

--- src/store-bundle.ts
+++ src/store-bundle.ts
@@ -93,7 +93,11 @@ async function storeBundle() {
         process.exit(1);
     }
 
-    console.log(bundle.getIds().map((id, index) => ({ file: files[index], media: `https://arweave.net/${id}`})));
+    console.log(bundle.getIds().map((id, index) => ({
+        file: files[index], 
+        media: `${argv.local ? 'http://127.0.0.1:1984/' : 'https://arweave.net/'}${id}`}
+        ))
+    );
     console.log("Transaction sent: " + response.status);
     console.log("Transaction ID: " + transaction.id);
 }
